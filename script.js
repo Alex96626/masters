fa39f4ff91a86d68cc2ccdc9bf832903e48c3d00
Alex96626 <work.alex966@gmail.com>
2023-10-02 15:53:55 +0300

    add master page

---
+++ script.js
@@ -0,0 +1,65 @@
+document.addEventListener('DOMContentLoaded', ()=> {
+    
+     const portfolioSliderSmall = new Swiper('.portfolio-slider-small', {
+       // Default parameters
+       slidesPerView: 3,
+       spaceBetween: 13,
+       loop: true,
+       autoplay: true,
+       delay: 50,
+       // Responsive breakpoints
+       //   breakpoints: {
+       //     // when window width is >= 320px
+       //     320: {
+       //       slidesPerView: 2,
+       //       spaceBetween: 20,
+       //     },
+       //     // when window width is >= 480px
+       //     480: {
+       //       slidesPerView: 3,
+       //       spaceBetween: 30,
+       //     },
+       //     // when window width is >= 640px
+       //     640: {
+       //       slidesPerView: 4,
+       //       spaceBetween: 40,
+       //     },
+       //   },
+     });
+
+    const swiper = new Swiper('#portfolio-slider-big', {
+      // Default parameters
+    //   slidesPerView: 5,
+    //   spaceBetween: 17,
+      loop: true,
+      autoplay: true,
+      delay: 50,
+      // Responsive breakpoints
+      breakpoints: {
+        1560: {
+          slidesPerView: 5,
+          spaceBetween: 17,
+        },
+
+        1200: {
+          slidesPerView: 4,
+          spaceBetween: 17,
+        },
+
+        900: {
+          slidesPerView: 3,
+          spaceBetween: 17,
+        },
+
+        560: {
+          slidesPerView: 2,
+          spaceBetween: 17,
+        },
+
+        0: {
+          slidesPerView: 1,
+          spaceBetween: 17,
+        }
+      },
+    });
+})
