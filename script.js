fe05f32b8ea4cdcbb642b7d5711871ea48b32fb6
Alex96626 <work.alex966@gmail.com>
2023-10-05 13:17:31 +0300

    fix

--- script.js
+++ script.js
@@ -20,23 +20,10 @@ document.addEventListener('DOMContentLoaded', ()=> {
 
          320: {
            slidesPerView: 1,
-           spaceBetween: 13,
+           spaceBetween: 5,
+          //  width: 220,
+           centeredSlides: true,
          },
-         //     // when window width is >= 320px
-         //     320: {
-         //       slidesPerView: 2,
-         //       spaceBetween: 20,
-         //     },
-         //     // when window width is >= 480px
-         //     480: {
-         //       slidesPerView: 3,
-         //       spaceBetween: 30,
-         //     },
-         //     // when window width is >= 640px
-         //     640: {
-         //       slidesPerView: 4,
-         //       spaceBetween: 40,
-         //     },
        },
      });
 
